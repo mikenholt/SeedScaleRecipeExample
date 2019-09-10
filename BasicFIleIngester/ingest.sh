747a3d7d801585e195c832efe7a8b98230b3b51b
Mike Holt <mike.n.holt@gmail.com>
2019-09-10 13:24:16 -0400

    Update ingest.sh

--- BasicFIleIngester/ingest.sh
+++ BasicFIleIngester/ingest.sh
@@ -2,6 +2,10 @@ echo "i found something..."
 echo ""
 echo ${INPUT_FILE}
 echo "what is this junk?"
+echo ""
+echo "lets see if we have any metadata about this file..."
+echo ${INPUT_METADATA}
+echo ""
 echo "where would i put it?"
 echo "${OUTPUT_DIR} ?"
 echo "no thanks...."
